35251c77a1054ff6e32261f6a27c243aeebb4997
SwethaDSalvatore <swetharamesh35309@gmail.com>
2023-09-14 22:28:34 +0530

    js file added

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const loginButton = document.querySelector("button");
+const emailInput = document.querySelector('input[type="email"]');
+const passwordInput = document.querySelector('input[type="password"]');
+const correctEmail = "user@example.com";
+const correctPassword = "password123";
+
+loginButton.addEventListener("click", function () {
+
+    // Check if the email and password are correct (you can replace this with your authentication logic)
+    if (emailInput.value === correctEmail && passwordInput.value === correctPassword) {
+        // Display a welcome message in the console
+        console.log(alert("welcome, " + emailInput.value));
+
+    } else {
+        // Show an error message or handle incorrect input as needed
+        console.log("Incorrect email or password. Please try again.");
+        console.log(alert("Please enter the correct value"));
+    }
+});
